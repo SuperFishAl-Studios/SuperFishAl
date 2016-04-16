49ab1b81183f1c5732e5f1e0cc291d7f8bd0034d
Brandon Salmon <brandon.j.salmon@gmail.com>
2016-04-16 14:40:10 -0500

    LockToY scene border

---
+++ SuperFishAl/Assets/Scripts/LockToY.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LockToY : MonoBehaviour
+{
+    public GameObject target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
+    }
+}
