04f304b6df8c2e05f6a447d4ed29a06ef531eae0
Brandon Salmon <brandon.j.salmon@gmail.com>
2016-04-16 14:23:06 -0500

    Generate rocks based on distance instead of time

--- SuperFishAl/Assets/Scripts/LevelGenerator.cs
+++ SuperFishAl/Assets/Scripts/LevelGenerator.cs
@@ -3,18 +3,30 @@
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject[] objects;
+    public float rate = 3;
+    public float width = 50;
+
+    private Vector3 previous;
+    private float distanceTraveled;
 
     // Use this for initialization
     void Start()
     {
+        distanceTraveled = 0.01f;
+        previous = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Random.value > .95)
+        distanceTraveled += (transform.position.y - previous.y);
+
+        if (Random.value * distanceTraveled > rate)
         {
-            Instantiate(this.objects[Random.Range(0, this.objects.GetLength(0))], new Vector3(transform.position.x + Random.Range(-10, 10), transform.position.y, 0), Quaternion.identity);
+            Instantiate(this.objects[Random.Range(0, this.objects.GetLength(0))], new Vector3(Random.Range(width / -2, width / 2), transform.position.y, 0), Quaternion.identity);
+            distanceTraveled -= rate;
         }
+
+        previous = transform.position;
     }
 }
