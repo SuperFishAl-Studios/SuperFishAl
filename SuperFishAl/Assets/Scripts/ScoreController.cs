1f3c43a3b30cb989103257eab4f75e0ae8dc3844
Brandon Salmon <brandon.j.salmon@gmail.com>
2016-04-17 18:40:07 -0500

    Score counter

--- SuperFishAl/Assets/Scripts/ScoreController.cs
+++ SuperFishAl/Assets/Scripts/ScoreController.cs
@@ -1,22 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
 {
     private int score;
     private Transform previous;
+    private Text Label;
 
     // Use this for initialization
     void Start()
     {
+        this.Label = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
         this.score = 0;
-        this.previous = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var deltaY = transform.position.y - this.previous.position.y;
-        this.score += (int)deltaY;
-        this.previous = transform;
+        this.score = (int)(transform.position.y - 50f);
+        this.Label.text = this.score.ToString();
     }
 }
