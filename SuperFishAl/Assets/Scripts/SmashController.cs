1d04cdef7b6644dd536705b58923bafae9bc72d0
Brandon Salmon <brandon.j.salmon@gmail.com>
2016-04-17 18:07:07 -0500

    Blow up rocks as Sharky

---
+++ SuperFishAl/Assets/Scripts/SmashController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SmashController : MonoBehaviour
+{
+    public GameObject Explosion;
+
+    private ParticleSystem particles;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.particles = this.Explosion.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.otherCollider.gameObject.transform.parent.tag == "Destructable")
+            {
+                var explode = Instantiate(this.Explosion, contact.otherCollider.transform.position, new Quaternion());
+                Destroy(contact.otherCollider.gameObject.transform.parent.gameObject);
+            }
+            else if (contact.otherCollider.tag == "Destructable")
+            {
+                var explode = Instantiate(this.Explosion, contact.otherCollider.transform.position, new Quaternion());
+                Destroy(contact.otherCollider.gameObject);
+            }
+            if (contact.collider.gameObject.transform.parent.tag == "Destructable")
+            {
+                var explode = Instantiate(this.Explosion, contact.collider.transform.position, new Quaternion());
+                Destroy(contact.collider.gameObject.transform.parent.gameObject);
+            }
+            if (contact.collider.tag == "Destructable")
+            {
+                var explode = Instantiate(this.Explosion, contact.collider.transform.position, new Quaternion());
+                Destroy(contact.collider.gameObject);
+            }
+        }
+    }
+}
