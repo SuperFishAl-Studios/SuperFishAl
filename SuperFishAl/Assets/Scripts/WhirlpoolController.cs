648136a70b240611b6b9f2d6646cad978178a77c
Brandon Salmon <brandon.j.salmon@gmail.com>
2016-04-17 19:10:55 -0500

    Whirlpool and improved level spawn quantities

---
+++ SuperFishAl/Assets/Scripts/WhirlpoolController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class WhirlpoolController : MonoBehaviour
+{
+    private Rigidbody2D Body;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.Body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Body.AddTorque(0.1f, ForceMode2D.Impulse);
+    }
+}
