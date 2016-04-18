595659540a271937849c7be346048247da6a77b0
Brandon Salmon <brandon.j.salmon@gmail.com>
2016-04-17 19:51:55 -0500

    Whirlpool chaos

--- SuperFishAl/Assets/Scripts/WhirlpoolController.cs
+++ SuperFishAl/Assets/Scripts/WhirlpoolController.cs
@@ -15,4 +15,21 @@ public class WhirlpoolController : MonoBehaviour
     {
         this.Body.AddTorque(0.1f, ForceMode2D.Impulse);
     }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Whirlpool" || other.gameObject.tag == "Boss" || other.gameObject.tag == "Enemy")
+        {
+            Destroy(other);
+            this.transform.localScale = new Vector3(this.transform.localScale.x * 1.20f, this.transform.localScale.x * 1.20f, this.transform.localScale.z);
+        }
+
+        other.GetComponent<Rigidbody2D>().AddForce((transform.position - other.transform.position) * 25, ForceMode2D.Impulse);
+
+        var health = other.transform.parent.GetComponent<HealthComponent>();
+        if (health != null)
+        {
+            health.DecreaseHealth(.01f);
+        }
+    }
 }
